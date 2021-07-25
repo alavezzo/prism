4f416096907445ea9c403bc9d453408f4c9a81ad
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-25 14:22:58 -0700

    added cocktail responsive elements - NOT DONE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,58 @@
-let apiUrlCocktail ="https://thecocktaildb.com/api/json/v1/1/search.php?s="+ drinkName;
-let cocktail = $("#cocktail").value
-fetch(apiUrlCocktail)
+let cocktail="";
+
+$("#search").click(function () {
+    event.preventDefault();
+    let cocktail = $("#cocktail").val();
+    if (cocktail) {
+        displayCocktail(cocktail)
+    };
+});
+
+
+let displayCocktail=function(cocktail){
+    let apiUrlCocktail ="https://thecocktaildb.com/api/json/v1/1/search.php?s="+ cocktail;
+    fetch(apiUrlCocktail)
     .then(function(response){
         return response.json()
     })
     .then(function(response){
-        console.log(response)
+        console.log(response.drinks[0])
+        $("#cocktail-recipe").empty();
+        let cocktailNameEl=$("<h4>");
+        let ingredientList=$("<ul>");
+        let ingredientsEl=$("<li>");
+        let instructionsEl=$("<p>");
+        let cocktailName= response.drinks[0].strDrink;
+        let cocktailImg=$("<img>");
+        cocktailImg.attr("src",response.drinks[0].strDrinkThumb+"/preview")
+        let ingredient1=response.drinks[0].strIngredient1;
+        let ingredient2=response.drinks[0].strIngredient2;
+        let ingredient3=response.drinks[0].strIngredient3;
+        let ingredient4=response.drinks[0].strIngredient4;
+        let ingredient5=response.drinks[0].strIngredient5;
+        let ingredient6=response.drinks[0].strIngredient6;
+        let ingredient7=response.drinks[0].strIngredient7;
+        let ingredient8=response.drinks[0].strIngredient8;
+        let ingredient9=response.drinks[0].strIngredient9;
+        let ingredient10=response.drinks[0].strIngredient10;
+        let ingredient11=response.drinks[0].strIngredient11;
+        let ingredient12=response.drinks[0].strIngredient12;
+        let ingredient13=response.drinks[0].strIngredient13;
+        let ingredient14=response.drinks[0].strIngredient14;
+        let ingredient15=response.drinks[0].strIngredient15;
+        let instructions=response.drinks[0].strInstructions;
+        cocktailNameEl.text(cocktailName);
+        //ingredients goes here
+        instructionsEl.text(instructions);
+        
+        $("#cocktail-recipe").append(cocktailImg);
+        $("#cocktail-recipe").append(cocktailNameEl);
+        $("#cocktail-recipe").append(ingredientList);
+        $("#cocktail-recipe").append(instructionsEl);
     });
+};
+
+
 
 let blues = '8055125582';
 let jazz = '151692581';
